65c020ad9a524ae25b318f93d6f80d700658ccad
Mark Rydstrom <mark.rydstrom@octopus.com>
2017-01-23 10:56:23 +1000

    Simple program to poke the nuget libs

---
+++ NugetTest/NugetAdaptor.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using NuGet.Common;
+using NuGet.Configuration;
+using NuGet.Protocol;
+using NuGet.Protocol.Core.Types;
+
+namespace NugetTest
+{
+    public class NugetAdaptor
+    {
+        private readonly SourceRepository _repository;
+
+        public NugetAdaptor(string url, string username, string password)
+        {
+            var source = new PackageSource(url, "my-feed");
+            if (!string.IsNullOrEmpty(username))
+            {
+                source.Credentials = new PackageSourceCredential(source.Name, username, password, isPasswordClearText: true);
+            }
+            _repository = new SourceRepository(source, Repository.Provider.GetCoreV3());
+        }
+
+        public void CreateReleaseTest(string packageId)
+        {
+            var task = _repository
+                .GetResource<PackageMetadataResource>(CancellationToken.None)
+                .GetMetadataAsync(packageId, false, false, NullLogger.Instance, CancellationToken.None);
+
+            task.Wait();
+
+            Console.WriteLine("{0} results found", task.Result.Count());
+        }
+    }
+}
--- NugetTest/Program.cs
+++ NugetTest/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NDesk.Options;
 
 namespace NugetTest
 {
@@ -10,6 +11,45 @@ namespace NugetTest
     {
         static void Main(string[] args)
         {
+           var program = new Program();
+            program.Execute(args);
         }
+
+        void Execute(string[] args)
+        {
+            bool showHelp = false;
+            var options = new OptionSet
+            {
+                { "url=", u => _feedUrl = u },
+                { "username=", n => _feedUsername = n },
+                { "password=", n => _feedPassword = n },
+                { "package=", v => _packageId = v },
+                { "h|help", _ => showHelp = true }
+            };
+
+            options.Parse(args);
+
+            if (showHelp)
+            {
+                options.WriteOptionDescriptions(Console.Out);
+                return;
+            }
+
+            var t = Enumerable.Range(0, 20)
+               .AsParallel()
+               .Select(_ => Task.Run(() =>
+               {
+                   var adaptor = new NugetAdaptor(_feedUrl, _feedUsername, _feedPassword);
+                   adaptor.CreateReleaseTest(_packageId);
+               }))
+               .ToArray();
+
+            Task.WaitAll(t);
+        }
+
+        string _feedUrl = "https://octopus.myget.org/F/octopus-dependencies/api/v2";
+        string _feedUsername = "";
+        string _feedPassword = "";
+        string _packageId = "halibut";
     }
 }
